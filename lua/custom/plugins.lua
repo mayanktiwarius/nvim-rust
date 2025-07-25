7d1e6fec58ee4ca009c5d608883b7b56ff83cccf
mayanktiwarius <mayank_tiwari@cable.comcast.com>
2025-07-25 17:00:47 -0400

    Sample for java

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -11,6 +11,22 @@ return {
     }
   },
   {
+    {
+      "nvim-java/nvim-java",
+      dependencies = {
+        "nvim-java/lua-async-await",
+        "nvim-java/nvim-java-core",
+        "nvim-java/nvim-java-test",
+        "nvim-java/nvim-java-dap",
+        "nvim-java/nvim-java-refactor",
+      },
+      config = function()
+        require("java").setup()
+      end,
+      ft = { "java" }, -- Only load for Java files
+    },
+  },
+  {
       "LunarVim/bigfile.nvim",
       config = function()
           require("bigfile").setup({

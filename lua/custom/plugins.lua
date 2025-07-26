08992699435df4f17a59d8975d4d0dbe01a582f5
mayanktiwarius <mayank_tiwari@cable.comcast.com>
2025-07-26 17:08:32 -0400

    Enabled noice

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -571,34 +571,34 @@ autocmd FileType proto ClangFormatAutoEnable
   --     require("diffview").setup({})
   --   end
   -- },
-  -- {
-  --     "folke/noice.nvim",
-  --     event = "VeryLazy",
-  --     dependencies = {
-  --         "MunifTanjim/nui.nvim",
-  --         "rcarriga/nvim-notify",
-  --         -- "nvim-treesiter/nvim-treesiter",
-  --         "nvim-treesitter/nvim-treesitter",
-  --     },
-  --     config = function()
-  --         require("noice").setup({
-  --             lsp = {
-  --                 override = {
-  --                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-  --                     ["vim.lsp.util.stylize_markdown"] = true,
-  --                     -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-  --                 },
-  --             },
-  --             presets = {
-  --                 bottom_search = false,
-  --                 command_palette = false,
-  --                 long_message_to_split = true,
-  --                 inc_rename = false,
-  --                 lsp_doc_border = false,
-  --             },
-  --         })
-  --     end
-  -- },
+  {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      dependencies = {
+          "MunifTanjim/nui.nvim",
+          "rcarriga/nvim-notify",
+          -- "nvim-treesiter/nvim-treesiter",
+          "nvim-treesitter/nvim-treesitter",
+      },
+      config = function()
+          require("noice").setup({
+              lsp = {
+                  override = {
+                      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                      ["vim.lsp.util.stylize_markdown"] = true,
+                      -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                  },
+              },
+              presets = {
+                  bottom_search = false,
+                  command_palette = false,
+                  long_message_to_split = true,
+                  inc_rename = false,
+                  lsp_doc_border = false,
+              },
+          })
+      end
+  },
   {
       'kevinhwang91/nvim-ufo',
       dependencies = { 'kevinhwang91/promise-async' },

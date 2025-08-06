1b3bbdc706146c1d94160ff3c2ebe42242e2e3d5
mayanktiwarius <mayank_tiwari@cable.comcast.com>
2025-08-06 10:57:05 -0400

    Revert "CPP support"
    This reverts commit 9e2d4682dc4b1bda0435e206cb953185d317ef2e.

--- init.lua
+++ init.lua
@@ -112,29 +112,3 @@ require("mason-lspconfig").setup {
   ensure_installed = { "terraformls", "tflint" }
 }
 
-return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.clangd.setup {
-        cmd = { "clangd", "--background-index", "--clang-tidy" },
-        filetypes = { "c", "cpp", "objc", "objcpp" },
-        root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
-      }
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = { "clangd" },
-      }
-    end,
-  },
-}
-
